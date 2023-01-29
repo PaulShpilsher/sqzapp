736cc73e1aa8694051d7e957824838b70cbc1bc0
Paul Shpilsher <paul.shpilsher@gmail.com>
2023-01-29 17:28:02 +0300

    move to LongUrlSubmit component

--- src/lib/Home.svelte
+++ src/lib/Home.svelte
@@ -1,19 +1,14 @@
+<script lang="ts">
+  import LongUrlSubmit from "./components/LongUrlSubmit.svelte";
+
+</script>
+
 <header />
 
-<div class="long-url-input">
-  <input type="url" class="long-url" placeholder="Enter a URL you'd like to shorten" />
-  <button type="button">Sqeeze</button>
-</div>
+<LongUrlSubmit />
 
 <div class="short-url-result">
   <div id="a-link">
     <a href="http://abc.xyz">abc.xyz</a>
   </div>
 </div>
-
-
-<style>
-    .long-url {
-        width: 300px;
-    }
-</style>
---
+++ src/lib/components/LongUrlSubmit.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+</script>
+
+<div class="long-url-input">
+  <input
+    type="url"
+    class="long-url"
+    placeholder="Enter a URL you'd like to shorten"
+  />
+  <button type="button">Sqeeze</button>
+</div>
+
+<style>
+    .long-url {
+        width: 300px;
+    }
+</style>
