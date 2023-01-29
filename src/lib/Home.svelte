21470fe968005b87114e01812353473faa0e1072
Paul Shpilsher <paul.shpilsher@gmail.com>
2023-01-29 17:05:37 +0300

    home page draft

---
+++ src/lib/Home.svelte
@@ -0,0 +1,19 @@
+<header />
+
+<div class="long-url-input">
+  <input type="url" class="long-url" placeholder="Enter a URL you'd like to shorten" />
+  <button type="button">Sqeeze</button>
+</div>
+
+<div class="short-url-result">
+  <div id="a-link">
+    <a href="http://abc.xyz">abc.xyz</a>
+  </div>
+</div>
+
+
+<style>
+    .long-url {
+        width: 300px;
+    }
+</style>
